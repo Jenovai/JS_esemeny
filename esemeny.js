c92efac38ecf0d267eff4b2d79c84db9611205e7
Jenovai <andrea.jenovai@gmail.com>
2021-03-23 15:23:11 +0100

    szoveg + stilusbeállitás modositása JS-böl

--- esemeny.js
+++ esemeny.js
@@ -17,3 +17,10 @@ function normal(){
     let normal_kep = document.querySelector("img")
     normal_kep.style.width = "50px"
 }
+
+let szoveg = document.getElementById("szoveg")
+szoveg.style.border = "black 5px solid"
+szoveg.style.backgroundColor = "rgba(179, 179, 179, 0.7)"
+szoveg.style.color = "red"
+szoveg.style.width = "80%"
+szoveg.style.margin = "auto"
